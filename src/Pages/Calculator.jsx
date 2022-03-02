b7416082c9e52d6800d701ad1046f427ab986496
Udit-Gogia <gogiaudit17@gmail.com>
2022-03-02 14:48:47 +0530

    Calculator UI done

--- src/Pages/Calculator.jsx
+++ src/Pages/Calculator.jsx
@@ -1,49 +1,76 @@
 import React from "react";
+import "../Styles/CalculatorCSS.css";
 
 function Calculator() {
-	return (
-		<div className="bg-[#1F1D21] w-screen h-screen text-[#DEDAE1] flex">
-			<div className="flex flex-col bg-[#DEDAE1] h-screen p-5 S font-bold text-[#476C9B] text-2xl w-[11vw]">
-				<p className="p-4">Sidebar</p>
-				<p className="p-4">Sidebar</p>
-				<p className="p-4">Sidebar</p>
-				<p className="p-4">Sidebar</p>
-			</div>
 
-			<div className="flex flex-col ">
-				<div className="bg-[#3d3b40] p-6 text-9xl  w-[89vw] flex justify-end">
-					980
+const 
+
+	return (
+		<div className="bg-[#1f1d21] w-screen h-screen p-4">
+			<div className="grid pt-8 text-transparent text-white text-5xl grid-props gap-4 justify-center">
+				<div
+					id="output "
+					className="col-span-full  bg-[#DEDAE1] flex text-black flex-col justify-around p-3break-words p-4"
+				>
+					<div id="prev" className="text-2xl"></div>
+					<div id="curr" className="text-5xl"></div>
 				</div>
-				<div className="flex">
-					<div className="flex flex-col">
-						<div className="flex p-4 ">
-							<p className="p-8 w-34 text-3xl">7</p>
-							<p className="p-8 w-34 text-3xl">8</p>
-							<p className="p-8 w-34 text-3xl">9</p>
-						</div>
-						<div className="flex p-4">
-							<p className="p-8 w-34 text-3xl">4</p>
-							<p className="p-8 w-34 text-3xl">5</p>
-							<p className="p-8 w-34 text-3xl">6</p>
-						</div>
-						<div className="flex p-4">
-							<p className="p-8 w-34 text-3xl">1</p>
-							<p className="p-8 w-34 text-3xl">2</p>
-							<p className="p-8 w-34 text-3xl">3</p>
-						</div>
-						<div className="flex p-4">
-							<p className="p-8 w-34 text-3xl">0</p>
-							<p className="p-8 w-34 text-3xl">.</p>
-							<p className="p-8 w-34 text-3xl">DEL</p>
-						</div>
-					</div>
-					<div className="flex">
-						<div></div>
-						<div></div>
-					</div>
-				</div>
+
+				<button className="border-2 rounded-lg text-white hover:bg-[#DEDAE1] hover:text-[#1f1d21] col-span-2 text-6xl">
+					AC
+				</button>
+				<button className="border-2 rounded-lg  text-6xl text-white hover:bg-[#DEDAE1] hover:text-[#1f1d21]">
+					C
+				</button>
+				<button className="border-2 rounded-lg  text-6xl text-white hover:bg-[#DEDAE1] hover:text-[#1f1d21]">
+					÷
+				</button>
+				<button className="border-2 rounded-lg  text-6xl text-white hover:bg-[#DEDAE1] hover:text-[#1f1d21]">
+					7
+				</button>
+				<button className="border-2 rounded-lg  text-6xl text-white hover:bg-[#DEDAE1] hover:text-[#1f1d21] ">
+					8
+				</button>
+				<button className="border-2 rounded-lg  text-6xl text-white hover:bg-[#DEDAE1] hover:text-[#1f1d21]">
+					9
+				</button>
+				<button className="border-2 rounded-lg  text-6xl text-white hover:bg-[#DEDAE1] hover:text-[#1f1d21]">
+					*
+				</button>
+				<button className="border-2 rounded-lg  text-6xl text-white hover:bg-[#DEDAE1] hover:text-[#1f1d21]">
+					4
+				</button>
+				<button className="border-2 rounded-lg  text-6xl text-white hover:bg-[#DEDAE1] hover:text-[#1f1d21]">
+					5
+				</button>
+				<button className="border-2 rounded-lg  text-6xl text-white hover:bg-[#DEDAE1] hover:text-[#1f1d21]">
+					6
+				</button>
+				<button className="border-2 rounded-lg  text-6xl text-white hover:bg-[#DEDAE1] hover:text-[#1f1d21]">
+					+
+				</button>
+				<button className="border-2 rounded-lg  text-6xl  text-white hover:bg-[#DEDAE1] hover:text-[#1f1d21]">
+					1
+				</button>
+				<button className="border-2 rounded-lg  text-6xl  text-white hover:bg-[#DEDAE1] hover:text-[#1f1d21]">
+					2
+				</button>
+				<button className="border-2 rounded-lg  text-6xl  text-white hover:bg-[#DEDAE1] hover:text-[#1f1d21]">
+					3
+				</button>
+				<button className="border-2 rounded-lg  text-6xl  text-white hover:bg-[#DEDAE1] hover:text-[#1f1d21]">
+					-
+				</button>
+				<button className="border-2 rounded-lg  text-6xl  text-white hover:bg-[#DEDAE1] hover:text-[#1f1d21]">
+					.
+				</button>
+				<button className="border-2 rounded-lg  text-6xl  text-white hover:bg-[#DEDAE1] hover:text-[#1f1d21]">
+					0
+				</button>
+				<button className="border-2 rounded-lg  text-6xl col-span-2 text-white hover:bg-[#DEDAE1] hover:text-[#1f1d21]">
+					=
+				</button>
 			</div>
-			<div></div>
 		</div>
 	);
 }
